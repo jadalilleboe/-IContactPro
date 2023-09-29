2eb3d9d3f42a8f6b227f7ba01dee3261ea55264b
Hannah Burns <hannahburns787@gmail.com>
2023-09-29 00:01:28 -0400

    i think this is initialization

--- ContactClass.swift
+++ ContactClass.swift
@@ -2,23 +2,40 @@ import Foundation
 import PersonClass
 
 class Contact: Person {
-    // Properties
+    // Properties specific to Contact
     var contactID: Int
     var tags: [CommunityTags]
     var notes: String
-    
-    // Initializer
+
+    // Initializer for Contact
     init(contactID: Int,
-         tags: [CommunityTags]) {
+         firstName: String,
+         lastName: String,
+         companyName: String,
+         jobTitle: String,
+         phoneNumbers: [PhoneNumber],
+         emailAddresses: [EmailAddress],
+         addresses: [Address],
+         socialProfiles: [SocialProfile],
+         tags: [CommunityTags],
+         notes: String) {
         self.contactID = contactID
         self.tags = tags
         self.notes = notes
-    }
 
-    func getTag(Int pos) -> String {
-        return "\(tags[pos])"
+        // Call the superclass initializer to set Person properties
+        super.init(firstName: firstName,
+                   lastName: lastName,
+                   companyName: companyName,
+                   jobTitle: jobTitle,
+                   phoneNumbers: phoneNumbers,
+                   emailAddresses: emailAddresses,
+                   addresses: addresses,
+                   socialProfiles: socialProfiles)
     }
 }
+
+
 
 // Define related structs or classes for nested properties (e.g., PhoneNumber, EmailAddress, Address, SocialProfile, CustomField)
 
